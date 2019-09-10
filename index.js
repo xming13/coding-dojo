e8406d892777acc341f39696e6e01c3859a1c263
weiseng <weiseng@redairship.com>
2019-09-10 12:05:20 +0800

    Implement solution

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const answer = () => {
-	throw 'No solution provided!';
+const answer = (number) => {
+  if (number % 3 === 0 && number % 5 === 0) {
+    return 'FizzBuzz'
+  }
+
+  if (number % 3 === 0) {
+    return 'Fizz'
+  }
+
+  if (number % 5 === 0) {
+    return 'Buzz'
+  }
+
+	return number
 }
 export default answer;
